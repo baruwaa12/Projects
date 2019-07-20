b0303cecf3722871edced82aada0a58f23572817
baruwaa12 <45532744+baruwaa12@users.noreply.github.com>
2019-07-20 20:10:42 +0100

    Add files via upload

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -1,38 +1,61 @@
 import random
 
-# known_number = input(int('Enter a number between 0 and 100'))
+number_guessed = int(input('Enter a number between 0 and 100'))
 
 Minimum = 0
 Maximum = 100
-number = random.randint(0, 100)
-Found = False
-attempt = 0
+not_Found = True
 
-while Found:
-    print("Is it %s?") str(number)
-    Answer = raw_input()
-    if 'no' in Answer.lower() and 'higher' in Answer.lower():
-        number -= random.randint(1, 4)
-    elif 'no' in Answer.lower() and 'higher' in Answer.lower():
-        number += random.randint(1, 4)
-    elif Answer.lower() == 'no'
-        print("Higher or lower?")
-        Answer = raw_input()
-        if Answer.lower() == 'higher'
-            number += random.randint(1, 4)
-        elif Answer.lower() == 'lower':
-            number -= random.randint(1, 4)
-    elif Answer.lower() == 'yes':
-        if attempt < 2:
-            print('Yes it only took me %s tries') % str(attempt)
-        elif attempt < 2 and attempt < 10:
-            print("Pretty good for a robot")
-        else:
-            print("That's so bad, % tries")
-        Found = False
-    Attempt += 1
 
-print('Thanks for playing the game')
+def looper(guess, maximum_number, attempts):
+    attempts = attempts + 1
+    print(f'Checking {guess} against {maximum_number}')
+    if guess == maximum_number:
+        print('Found')
+        return attempts
+    x = int(maximum_number/2)
+    if guess > x :
+        new_max = int(((maximum_number - x)/2)+ x)
+        print(new_max)
+        looper(guess, new_max, attempts)
+    else:
+        if guess == x:
+            print('Found')
+            return attempts
+        new_max = int(((maximum_number - guess)/2)+ x)
+        print(new_max)
+        looper(guess, new_max, attempts)
+
+
+result = looper(number_guessed, 100, 0)
+print(f'Found in {result} attempt')
+
+
+
+# while not_Found: 
+#     Answer = number_guessed
+#     if 'no' in Answer.lower() and 'higher' in Answer.lower():
+#         number -= random.randint(1, 4)
+#     elif 'no' in Answer.lower() and 'higher' in Answer.lower():
+#         number += random.randint(1, 4)
+#     elif Answer.lower() == 'no':
+#         print("Higher or lower?")
+#         Answer = input()
+#         if Answer.lower() == 'higher':
+#             number += random.randint(1, 4)
+#         elif Answer.lower() == 'lower':
+#             number -= random.randint(1, 4)
+#     elif Answer.lower() == 'yes':
+#         if attempt < 2:
+#             print('Yes it only took me %s tries' % str(attempt))
+#         elif attempt < 2 and attempt < 10:
+#             print("Pretty good for a robot")
+#         else:
+#             print("That's so bad, % tries")
+#         Found = False
+#     attempt += 1
+
+# print('Thanks for playing the game')
 
     
 
