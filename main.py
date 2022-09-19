7f52c15fbf17390de78f15e1881c37b89d0b913b
baruwaa12 <45532744+baruwaa12@users.noreply.github.com>
2022-09-19 19:00:37 +0000

    Completed tic tac toe. Will attempt to write on my own next.

--- main.py
+++ main.py
@@ -76,16 +76,68 @@ def coordinates(user_input):
     return (row, col)
   
 
+def add_to_board(coords, board, active_user):
+  row = coords[0]
+  col = coords[1]
+  board[row][col] = active_user
+
+def current_user(user):
+  if user: return "x"
+  else: return "o"
+
+def isWin(user, board):
+  if check_row(user, board): return True
+  if check_col(user, board): return True
+  if check_diag(user, board): return True
+  return False
+
+
+def check_row(user, board):
+  for row in board:
+    complete_row = True
+    for slot in row:
+      if slot != user:
+        complete_row = False
+        break
+    if complete_row: return True
+  return False 
+
+def check_column(user, board):
+  for col in range(3):
+    complete_col = True
+    for row in range(3):
+      if board[row][col] != user:
+        complete_col = False
+        break
+    if complete_col: return True
+  return False
+
+def check_diag(user, board):
+  #top left to bottom right
+  if board[0][0] == user and board[1][1] == user and board[2][2] == user: return True
+  elif board[0][2] == user and board[1][1] == user and board[2][0] == user: return True
+  else: return False
+
 
   ## Loop which prints the board after each user input
-while True:
-  printBoard(board)
-  user_input = input("Please enter a position 1 through 9 or enter \"q\" to quit: ")
-  if quit(user_input):
-    break
-  if not check_Input(user_input):
+while turns < 9:
+  active_user = current_user(user)
+  print_board(board)
+  user_input = input("Please enter a position 1 through 9 or enter \"q\" to quit:")
+  if quit(user_input): break
+  if not check_input(user_input):
     print("Please try again.")
     continue
-  user_input = int(user_input)
+  user_input = int(user_input) - 1
   coords = coordinates(user_input)
-    
+  if istaken(coords, board):
+    print("Please try again.")
+    continue
+  add_to_board(coords, board, active_user)
+  if iswin(active_user, board): 
+    print(f"{active_user.upper()} won!")
+    break
+  
+  turns += 1
+  if turns == 9: print("Tie!")
+  user = not user
