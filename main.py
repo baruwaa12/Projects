0d9a7b833f24729095308332b28c1cde63e9414d
baruwaa12 <45532744+baruwaa12@users.noreply.github.com>
2022-10-09 11:03:49 +0000

    Still trying not sure what to do.

--- main.py
+++ main.py
@@ -1,26 +1,57 @@
 import random
-from collections import Counter
 
-cards = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+Spades = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+Hearts = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+Diamonds = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+Clubs = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
-# hand = random.sample(cards, 5)
-# print(hand)
+hand = random.sample(cards, 5)
+print(hand)
+
+
+# Create a 2D array to store all suits of cards
+
+
+# Function checks whether 2 lists have the same elements regardless of order
+def check_Same_contents(hand):
+
+  royalFlush = [cards[0], cards[8], cards[9], cards[10], cards[11], cards[12]]
+
+  for x in set(hand + royalFlush):
+    if hand.count(x) != royalFlush.count(x):
+        return False
+    return True
+
+
 
 # Function to check the type of hand that has been dealt
 def HandType(hand):
 
-  royalFlush = ["J", "10", "9", "8", "7"]
+  royalFlush = [cards[0], cards[8], cards[9], cards[10], cards[11], cards[12]]
 
   FourKind = ["2", "2", "2", "2", "2"]
 
   Straight = ["10", "9", "8", "7", "6"]
 
+  Flush = [] 
+  
+
+  
+  
   
     
   if hand == royalFlush:
-    print("You have a royal flush)
+    print("You have a royal flush")
 
-  if hand == flush
+  if hand == Straight:
+    print("You have a straight")
 
-BestHand(["J", "10", "9", "8", "7"])
+  if hand == FourKind:
+    print("You have four of a kind")
+
+  if hand == Flush:
+    print("You have a Flush")
+
+  
+HandType(hand)
 
